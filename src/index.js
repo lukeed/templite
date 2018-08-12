063b8bb341bd8801c44093d3cfc2bb2de4c88651
Luke Edwards <luke.edwards05@gmail.com>
2018-08-12 13:40:26 -0700

    fix: allow whitespace

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default function (str, mix) {
 	return str.replace(RGX, (x, key, y) => {
 		x = 0;
 		y = mix;
-		key = key.split('.');
+		key = key.trim().split('.');
 		while (y && x < key.length) {
 			y = y[key[x++]];
 		}
